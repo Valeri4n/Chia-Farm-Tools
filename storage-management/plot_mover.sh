f4d85d5ade93d627604760df250debfce4e1828e
Valerian <111314495+Valeri4n@users.noreply.github.com>
2023-03-04 09:06:00 -0700

    Update timer, source echo info, better handling
    - Added random timer to deconflict plot moves.
    - Updated output notification now aligns with source input.
    - Improved handling for cache in same mount location as destination folders. Uses nft_ in cache vs nft- in destinations. This will allow for local moving and prevent writing to cache drives.

--- storage-management/plot_mover.sh
+++ storage-management/plot_mover.sh
@@ -21,42 +21,44 @@ echo
 echo "Using plotter cache drive $1"
 echo "Plots will be moved to automatically fill all drives mounted in $2"
 echo "Simply run plotter continually plotting to cache location"
-echo "Run as many instances of this script as needed to move plots faster. Multiple instances will deconflict themselves on teh same machine."
+echo "Run as many instances of this script as needed to move plots faster. Multiple instances will deconflict themselves on the same machine."
+echo
+echo "Source location should have file name nft_SomeName. Each destination drive should have file named nft-SomeName."
+echo "Note the underscore vs the dash. Thsi is necessary to prevent trying to write to cache drive."
 echo
 
-CACHE=$1 # Source drive
-MOUNT=$2 # Destination directory
+CACHE=$1 # Source drive where the plotter leaves the plots
+MOUNT=$2 # Destination directory where the plots will be stored
 
 #k32 C0=108836000
 #k33 C0=230000000
 #k34 C0=462000000
 
-#Cache drive location - change as needed
-#CACHE=/svr/$1/cache$2
-# Farm drive location - change as needed. It will iterate all drives in this directory
-#MOUNT=/mnt
-
+# Determine the size of the cache drive for use calculation
 size_pointer=`ls $CACHE/drive-size-* 2>/dev/null|wc -l`
 if [ $((size_pointer)) -eq 0 ]; then
   size=`df $CACHE|awk '{print $2}'|sed -n 2p`
   touch $CACHE/drive-size-$size
 fi
 
-printf "\n"
+echo
 while true; do
   ls $CACHE/*.plot 2>/dev/null | while read plot; do
     # ----------------------------------------
     # Look to see if plots exist in cache drive. Don't proceed until they do.
     if [ -f $plot ]; then
       plot_name=`echo $plot|tail -c 96`
+      (( n = RANDOM % 30 )) # randomize the sleep timer 0-3 seconds
+      sleep_time=`printf '%s.%s\n' $(( n / 10 )) $(( n % 10 ))`
+      sleep ${sleep_time} # sleep timers to help prevent duplicate plot moves
       plot_moving=`ps aux | grep $plot_name | grep -v -e --color -e "grep" | wc -l`
       if [ $((plot_moving )) -eq 0 ]; then
-        NFT=$(basename $(find $CACHE -type f -name nft-* 2>/dev/null))
+        NFT=$(basename $(find $CACHE -type f -name nft_* 2>/dev/null)|sed 's/_/-/g')
         # determine the plot size
         plot_size=`du -k "$plot" | cut -f1`
         # ----------------------------------------
         # Look to see if the destination drive currently has any other plots actively being copied to it. If so, skip that drive.
-        for sd__ in $MOUNT/sd* ; do
+        for sd__ in $MOUNT/* ; do
           if [ "$(ls -la $sd__/.plot-* 2>/dev/null | wc -l)" -eq 0 ]; then # && [[ $NFT == ]]; then
             if [ -f $sd__/$NFT ]; then
               AVAIL=$(df $sd__ --output=avail | awk -F "[[:space:]]+" '{print $1}' | tail -n 1)
@@ -76,20 +78,21 @@ while true; do
           used=`du $CACHE|awk '{print $1}'|tail -1`
           size=`ls $CACHE/drive-size-*|awk -F "drive-size-" '{print $2}'`
           full=`printf "%.0f" $(awk "BEGIN {print $used/$size*100}")`
-          DT=`date +"%y-%m-%d"`; TM=`date +"%T"`
+          DT=`date +"%m-%d"`; TM=`date +"%T"`
           num_plots=`ls $CACHE/*.plot|wc -l`
           if [ $((num_plots)) -eq 1 ]; then mult=""; else mult="s"; fi
-          (( n = RANDOM % 30 ))
+          (( n = RANDOM % 50 )) # randomize the sleep timer 0-5 seconds
           sleep_time=`printf '%s.%s\n' $(( n / 10 )) $(( n % 10 ))`
-          sleep ${sleep_time}
+          sleep ${sleep_time} # sleep timers to help prevent duplicate plot moves
           plot_moving=`ps aux | grep $plot_name | grep -v -e --color | wc -l`
           if [ $((plot_moving )) -gt 1 ]; then break; fi
           tput_hi=`(tput setaf 3)`
           tput_lo=`(tput setaf 6)`
           tput_off=`(tput sgr0)`
-          printf "${tput_lo}\n\n$DT-$TM ${tput_hi}$full"%%" with $num_plots plot$mult${tput_lo} -> $finLOC\n${tput_off}"
+          printf "${tput_lo}\n\n$DT-$TM ${tput_hi}$full"%%" - $num_plots plot$mult${tput_lo} -> $finLOC $NFT\n${tput_off}"
+          # move the plot from cache to farm
           ls $plot | xargs -P1 -I% rsync -vhW --chown=$USER:$USER --chmod=0744 --progress --remove-source-files % $finLOC/
-          break
+          break # exit the loop after successfully moving plot
         else
           printf "No drive space found. Waiting 60 seconds to recheck.\n"
           sleep 10
@@ -98,14 +101,14 @@ while true; do
     else
       cnt=$(($cnt + 1))
       sp="-\|/"
-      DT=`date +"%y-%m-%d"`; TM=`date +"%T"`
-      printf "\r$DT $TM  \b${sp:cnt%${#sp}:1} no plots $1/cache$2"
+      DT=`date +"%m-%d"`; TM=`date +"%T"`
+      printf "\r$DT $TM  \b${sp:cnt%${#sp}:1} no plots $1 $NFT"
       sleep 0.2
     fi
   done
   cnt=$(($cnt + 1))
   sp="-\|/"
-  DT=`date +"%y-%m-%d"`; TM=`date +"%T"`
-  printf "\r$DT $TM  \b${sp:cnt%${#sp}:1} waiting $1/cache$2"
+  DT=`date +"%m-%d"`; TM=`date +"%T"`
+  printf "\r$DT $TM  \b${sp:cnt%${#sp}:1} waiting $1 $NFT"
   sleep 1
 done
