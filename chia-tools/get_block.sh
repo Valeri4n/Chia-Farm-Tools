41b450a5c78f7c40d1558d90455d9dfad2c26e8e
Valerian <111314495+Valeri4n@users.noreply.github.com>
2023-11-01 00:25:38 -0600

    Update get_block.sh
    Optimized script efficiency

--- chia-tools/get_block.sh
+++ chia-tools/get_block.sh
@@ -19,8 +19,11 @@
 #
 # This will check the last wallet synced. Check different wallet by syncing other wallet.
 
-echo "Commencing block lookup"
-reverse_block=9 # number to search backwards for proper block
+
+printf "Commencing block lookup\n\n"
+echo|chia wallet get_transactions > wallet_data
+chia rpc farmer get_harvesters > plot_data
+block_sub=20 # number to search backwards for proper block
 first_check=1
 if [[ -z $1 ]]; then
   last_check=1
@@ -28,72 +31,69 @@ elif [[ $1 = -o ]]; then # only this block
   last_check=$2
   first_check=$2
 elif [[ $1 = all ]]; then 
-  last_check=$(echo|chia wallet get_transactions|grep -B 3 rewarded|grep Transaction|awk -F"Transaction " '{print $2}'|wc -l)
+  last_check=$(grep -B 3 rewarded wallet_data|grep Transaction|awk -F"Transaction " '{print $2}'|wc -l)
 else
   last_check=$1
 fi
+block_txns=$(grep -B 2 -A 2 rewarded wallet_data|grep "Transaction"|awk -F"Transaction " '{print $2}')
+txn_times=$(grep -B 2 -A 2 rewarded wallet_data|grep "Created"|awk -F"Created at: " '{print $2}'|sed 's/ /,/')
 
-block_txns=$(echo|chia wallet get_transactions|grep -B 2 -A 2 rewarded|grep "Transaction"|awk -F"Transaction " '{print $2}')
-txn_times=$(echo|chia wallet get_transactions|grep -B 2 -A 2 rewarded|grep "Created"|awk -F"Created at: " '{print $2}'|sed 's/ /,/')
+i=1
+while read -r line; do
+  host_line[$i]=$line
+  i=$(($i+1))
+done <<<$(sed -n '/\<host\>/=' plot_data)
+i=1
+for line_number in "${host_line[@]}"; do
+  host_ip[$i]=$(sed "${line_number}q;d" plot_data|awk -F'\"' '{print $4}')
+  host[$i]=$(echo "${host_ip[$i]}"|awk -F. '{print $4}')
+  i=$(($i+1))
+done
 
 for (( check=${first_check}; check<=${last_check}; check++)); do
   wait_str=""
   last_block=$check
-  txn=$(echo $block_txns|awk -v str=$check '{print $str}')
-  txn_time=$(echo $txn_times|awk -v str=$check '{print $str}'|sed 's/,/ /')
+  txn=$(echo "${block_txns}"|sed -n ${check}p)
+  txn_time=$(echo "${txn_times}"|sed -n ${check}p|sed 's/,/ /')
   printf "\nPrevious block # ${last_block} at $txn_time\ntxn=${txn}\n"
   block=$(curl -s https://alltheblocks.net/chia/coin/0x${txn}|grep "Confirmed At Height"|awk -F"height=" '{print $2}'|awk -F\" '{print $2}')
-  block=$((${block}-${reverse_block}))
   plot=""
-  i=0
-  plot_found=true
+  plot_found=false
   nft=""
-
-  while [[ -z $plot ]]; do
-    i=$(($i+1))
-    block=$(($block+1))
+  block_less=$block_sub
+  while [[ -z $plot ]] && [[ ${block_less} -ge 0 ]]; do
     wait_str="${wait_str}."
     block_str="Checking block $block"
     printf "\r$block_str$wait_str"
     pkey=$(curl -s "https://alltheblocks.net/chia/height/${block}"|grep "Plot Public Key" -A 1|grep -v "Plot Public Key"|sed 's/ //g')
-    plot=$(chia rpc farmer get_harvesters|grep -B5 ${pkey}|grep -o '/.*\.plot')
-    if [[ ${i} -ge ${reverse_block} ]] && [[ -z $plot ]]; then
-      printf "\r             \n$(tput setaf 3)Plot not found$(tput sgr0)\n\n"
-      plot_found=false; break
+    if [[ $(cat plot_data|grep $pkey|wc -l) -gt 0 ]]; then
+      pkey_line=$(grep -n ${pkey} plot_data |awk -F: '{print $1}')
+      i=1
+      for line_number in "${host_line[@]}"; do
+        if [[ ${pkey_line} -lt ${line_number} ]] || [[ ${i} -eq ${#host_line[@]} ]]; then
+          j=$(($i-1))
+          plot_host_ip="${host_ip[$j]}"
+          plot_host="${host[$j]}"
+          plot=$(sed "$(($(grep -n ${pkey} plot_data |awk -F: '{print $1}') - 2))q;d" plot_data|awk -F'\"' '{print $4}')
+          plot_block=$block
+          plot_found=true
+          echo $plot_str
+          break
+        fi
+        i=$(($i+1))
+      done
     fi
+    block_less=$((${block_less}-1))
+    block=$(($block-1))
   done
 
-  if $plot_found; then
-    printf "\rplot=$plot\n"
-    svr=""
-    server=""
-    for i in {1..10}; do
-      if [[ ${i} -eq 1 ]] && [[ $(echo $plot|awk -F/ -v x=$i '{print $x}') == "" ]]; then
-        root=true
-      elif [[ ${i} -eq 1 ]]; then
-        root=false
-      fi
-      if [[ $(echo $plot|awk -F/ -v x=$i '{print $x}'|tail -c 6) == ".plot" ]]; then
-        plot=$(echo $plot|awk -F/ -v x=$i '{print $x}')
-        break
-      elif ([[ ${i} -eq 1 ]] && ! $root ) || ([[ ${i} -eq 2 ]] && $root ); then
-        mount_path=$(echo $plot|awk -F/ -v x=$i '{print $x}')
-        if [[ ${mount_path} == "svr" ]] || [[ ${mount_path} == "srv" ]]; then
-          svr=$(echo $plot|awk -F/ -v x=$((${i}+1)) '{print $x}')
-          if [[ ! -z $svr ]]; then
-            server=${svr}/
-          fi
-          drive=$(echo $plot|awk -F/ -v x=$((${i}+2)) '{print $x}')
-          nft=$(ls /${mount_path}/${server}${drive}/nft-* 2>/dev/null|awk -F/ -v x=$((${i}+3)) '{print $x}')
-        else
-          svr=$(hostname|awk -F- '{print $1}')
-          drive=$(echo $plot|awk -F/ -v x=$((${i}+1)) '{print $x}')
-          nft=$(ls /${mount_path}/${server}${drive}/nft-* 2>/dev/null|awk -F/ -v x=$((${i}+2)) '{print $x}')
-        fi
-      fi
-    done
+  if ! $plot_found; then
+    printf "\r  $(tput setaf 3)Plot not found$(tput sgr0)                               \n\n"
+  else
     color="$(tput setaf 5)"
     if [[ ! -z $nft ]]; then nft=${nft}-; fi
-    printf "${color}\n$txn_time - block=${block}\npkey=${pkey}\n${nft}${svr}-${plot}$(tput sgr0)\n\n"
+    printf "${color}\n$txn_time - block=${plot_block}\npkey=${pkey}\n//$plot_host_ip$plot$(tput sgr0)\n\n"
   fi
 done
+rm wallet_data
+rm plot_data
