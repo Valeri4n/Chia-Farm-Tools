7adfa60f589cd587e772c7181686cac6066a73e9
Valerian <111314495+Valeri4n@users.noreply.github.com>
2023-12-02 15:52:50 -0700

    Update write_drive_size.sh

--- storage-management/write_drive_size.sh
+++ storage-management/write_drive_size.sh
@@ -3,13 +3,13 @@
 # Copyright 2023 by Valerian
 
 # This script will add the drive size and nft poitn files to a drive
-# Inpout values for drive path, SRC drive True or False, and nft NAME
+# Inpout values for mountpoint, SRC drive True or False, and nft NAME
 
-path=
+mountpoint=/mnt/cache
 SRC=true
 nft=NAME
 
-drive=$(mount|grep $path)
+drive=$(mount|grep $mountpoint)
 if $SRC; then nft_sw="_"; else nft_sw="-"; fi
 space=$(df ${drive}|sed -n 2p|awk '{print $2}')
 size=$(lsblk -o size ${drive}|sed -n 2p|sed 's/ //g')
@@ -17,7 +17,7 @@ if [[ $(echo $size|tail -c 2) == G ]]; then
   size=$(bc <<< "scale=2 ; $(echo $size|head -c -2) / 1000")
   size=$(printf "%'0.2f" "$size")T
 fi
-touch ${path}/nft${nft_sw}${NFT}
-new_file=${path}/drive-size-${space}-${size}
+touch ${mountpoint}/nft${nft_sw}${NFT}
+new_file=${mountpoint}/drive-size-${space}-${size}
 touch $new_file
 echo "$new_file nft${nft_sw}${NFT}"
