582b45cb86ff2d46957bc95a09a6cdb6d28d8a5a
Valerian <111314495+Valeri4n@users.noreply.github.com>
2023-12-21 10:27:48 -0700

    Update log_scrub_bad_plots.sh

--- storage-management/log_scrub_bad_plots.sh
+++ storage-management/log_scrub_bad_plots.sh
@@ -6,12 +6,25 @@
 #   Exception fetching qualities for [plot]. badbit or failbit...
 # this script will delete them automatically
 
+remove_bad_plots(){
+  for plot in $(cat .chia/mainnet/log/debug.log|grep "${bad_str}"|awk -F"${sep_str1}" '{print $2}'|awk -F"${sep_str2}" '{print $1}'); do
+    echo $plot
+    if [[ $(ls $plot 2>/dev/null) > 0 ]]; then
+      cnt=$(($cnt + 1))
+      echo "Removing $cnt - $plot"
+      rm $plot
+    fi
+  done
+}
+
 cnt=0
-for plot in $(cat .chia/mainnet/log/debug.log|grep badbit|awk -F"qualities for " '{print $2}'|awk -F". " '{print $1}'); do
-  if [[ $(ls $plot 2>/dev/null) > 0 ]]; then
-    cnt=$(($cnt + 1))
-    echo "Removing $cnt - $plot"
-    rm $plot
-  fi
+lookup=("badbit or failbit") # "Invalid file ")
+separator1=("qualities for ") # "Invalid file ")
+separator2=(". ") # " ")
+for i in "${!lookup[@]}"; do
+  bad_str="${lookup[$i]}"
+  sep_str1="${separator1[$i]}"
+  sep_str2="${separator2[$i]}"
+  remove_bad_plots
 done
 echo "Deleted $cnt bad plots!"
