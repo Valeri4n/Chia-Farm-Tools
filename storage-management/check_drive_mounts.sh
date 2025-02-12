3fef2761e6e68fc7e1a65330a006f821e1546e21
Valerian <111314495+Valeri4n@users.noreply.github.com>
2025-02-12 07:24:51 -0700

    Create check_drive_mounts.sh

---
+++ storage-management/check_drive_mounts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Copyright 2025 by Valerian
+#
+# This will check drive mounts every X number of seconds.
+# If any drives are attached to the system and not mounted, 
+#   it will mount them automatically.
+# Change parameters as desired.
+
+type=ext4
+mount_path=/mnt/
+time=10
+
+while true; do
+  cnt=$(lsblk -o mountpoint,name,fstype|grep " sd.*${type}"|grep -v /mnt/|wc -l)
+  if [ $cnt -gt 0 ]; then
+    while IFS= read -r line; do
+      mount /dev/$line
+      echo "mounted $line at $(date)"
+    done < <( lsblk -o mountpoint,name,fstype|grep " sd.*${type}"|grep -v ${mount_path}|awk '{print $1}' )
+  fi
+  sleep ${time}
+done
