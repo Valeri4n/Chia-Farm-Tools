dd16b4f632a6bd80c57e31818a111de500dc891f
Valerian <111314495+Valeri4n@users.noreply.github.com>
2025-04-01 11:56:54 -0600

    Update 2-install_apps.sh

--- ubuntu-build/Install_components/2-install_apps.sh
+++ ubuntu-build/Install_components/2-install_apps.sh
@@ -25,27 +25,6 @@ initialize(){
   fi
 }
 
-setup_tmux(){
-  if [[ ! -f /home/${this_user}/.tmux.conf ]]; then
-    cp /usr/share/doc/tmux/example_tmux.conf /home/${this_user}/.tmux.conf
-    chown ${this_user}: /home/${this_user}/.tmux.conf
-  fi
-  if [[ $(cat /home/${this_user}/.tmux.conf|grep "set -g mouse on"|grep -v "#"|wc -l) -eq 0 ]]; then
-    echo "# Enable mouse mode" >> /home/${this_user}/.tmux.conf
-    echo "set -g mouse on" >> /home/${this_user}/.tmux.conf
-  fi
-}
-
-create_tmux_session(){
-  session=install
-  tmux has-session -t $session 2>/dev/null
-  if [ $? != 0 ]; then
-    tmux new-session -s $session
-  else
-    tmux a -t $session
-  fi
-}
-
 install_apps(){
   this_time=$(date +%y%m%d.%H:%M:%S)
   touch /home/${this_user}/.InintializingUbuntuBuild_apps_started-${this_user}-${this_time}
@@ -55,10 +34,7 @@ install_apps(){
   export DEBIAN_FRONTEND=noninteractive
   sudo apt update
   sudo NEEDRESTART_MODE=a apt dist-upgrade -y
-  sudo apt install -y ca-certificates curl gnupg samba cifs-utils smartmontools mdadm xfsprogs ledmon dos2unix ${tmux_install}${linux_image}
-  if [[ ! -z $tmux_install ]]; then
-    setup_tmux
-  fi
+  sudo apt install -y ca-certificates curl gnupg samba cifs-utils smartmontools mdadm xfsprogs ledmon dos2unix tmux ${linux_image}
   sudo smbpasswd -a $this_user # Only needed if using samba mounts
   touch /home/${this_user}/.InintializingUbuntuBuild_apps_finished-${this_user}-$(date +%y%m%d.%H:%M:%S)
   rm /home/${this_user}/.InintializingUbuntuBuild_apps_started-${this_user}-${this_time}
@@ -71,8 +47,4 @@ install_apps(){
 
 check_root
 initialize
-if [[ $(which tmux|wc -l) -ge 1 ]]; then
-  setup_tmux
-  create_tmux_session
-fi
 install_apps
