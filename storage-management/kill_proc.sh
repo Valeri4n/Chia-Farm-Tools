37dfd4133668320dd7ac759491c6f94616b5f929
Valerian <111314495+Valeri4n@users.noreply.github.com>
2025-04-10 16:06:29 -0600

    Create kill_proc.sh

---
+++ storage-management/kill_proc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dev=$1
+sleep 5
+PID=$(ps aux|grep "fsck -CTn ${dev}"|grep -v -e grep|sed -n 1p|awk '{print $2}')
+psaux=$(ps aux|grep "fsck -CTn ${dev}"|grep -v -e grep)
+if [[ ! -z $PID ]]; then
+  kill -9 ${PID}
+fi
